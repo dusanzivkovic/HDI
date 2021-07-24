156273344a2157b23a5363cca3ad53a3bc0ad103
Dusan <dusziv@mts.rs>
2021-07-24 12:05:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Human Development Index (HDI)
+
+- A map showing Human Development Index (HDI)
+- Created using [Mapbox-Choropleth](https://github.com/stevage/mapbox-choropleth) and [Map-GL-Utils](https://github.com/stevage/map-gl-utils) plugins 
+- Live [demo](https://awesome-euclid-4d470f.netlify.app/)
---
+++ script.js
@@ -0,0 +1,26 @@
+const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/voyager/style.json?key=FUWC63pC4JOppUGhyT5X',
+    center: [7, 20], 
+    zoom: 1.8,
+});
+
+U.init(map)
+
+map.U.onLoad(() => {
+    map.U.addGeoJSON('states', 'countries.geojson')
+    map.U.addFillLayer('states', 'states', {fillColor: 'rgba(0, 0, 0, 0)', fillOutlineColor: 'rgba(212, 212, 212, 1)'})
+})
+
+const c = new MapboxChoropleth({
+    tableUrl: 'csvData.csv',
+    tableNumericField: 'hdi',
+    tableIdField: 'country',
+    geometryUrl: 'countries.geojson',
+    geometryIdField: 'SOVEREIGNT',
+    binCount: 7,
+    colorScheme: 'BuPu',
+    legendElement: '#legend',
+})
+
+c.addTo(map)
